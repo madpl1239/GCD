da4101500a33548a12f6c14f6ffbb7f5005b8c5f
madpl1239 <132273190+madpl1239@users.noreply.github.com>
2024-11-17 09:02:00 +0100

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -13,20 +13,20 @@ int main(void)
 {
 	int a, b, c, d = 0;
     
-    // logo
-    std::cout << "GCD v1.00 by madpl 2018\n\n";
+	// logo
+	std::cout << "GCD v1.00 by madpl 2018\n\n";
 
-    std::cout << "enter a: ";
-    std::cin >> a;
-    std::cout << "enter b: ";
-    std::cin >> b;
+	std::cout << "enter a: ";
+	std::cin >> a;
+	std::cout << "enter b: ";
+	std::cin >> b;
 
-    c = gcdEuclide(a, b);
-    d = gcdRec(a, b);
-    std::cout << "gcdEuclide(" << a << ", " << b << ") = " << c << std::endl;
-    std::cout << "gcdRec(" << a << ", " << b << ") = " << c << std::endl;
+	c = gcdEuclide(a, b);
+	d = gcdRec(a, b);
+	std::cout << "gcdEuclide(" << a << ", " << b << ") = " << c << std::endl;
+	std::cout << "gcdRec(" << a << ", " << b << ") = " << c << std::endl;
 
-    std::cout << "done. \n";
+	std::cout << "done. \n";
 
 	return 0;
 }
